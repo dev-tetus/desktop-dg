5fecef52c7e61b64298e843bdf4bbb6da424580d
Teo Roca <teo.roca798@gmail.com>
2021-01-14 11:41:37 +0100

    añade rutas navegacion y estilos basicos aplicacion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import "./App.css";
+import "./App.scss";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+
+import { Home, Login } from "./pages/";
 
 function App() {
-  return <div className="App">Hellow world!!!!</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" exact component={Login} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
