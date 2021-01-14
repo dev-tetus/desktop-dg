743b1781313830aa2aa2adef7a2b6c9396488045
Teo Roca <teo.roca798@gmail.com>
2021-01-14 13:24:17 +0100

    añade logica autenticado al cargar la pagina y redirect a login

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,38 @@
-import React from "react";
+import { logDOM } from "@testing-library/react";
+import React, { useEffect, useState } from "react";
+import { Redirect } from "react-router-dom";
+
+import axios from "../config/axios";
 
 function Home() {
-  return <div>This is home!</div>;
+  const [loggedIn, setLoggedIn] = useState(false); //* Local state for auth
+  //TODO Redux state for auth
+  useEffect(async () => {
+    try {
+      const response = await axios({
+        method: "get",
+        url: "/auth/session",
+      });
+      setLoggedIn(true);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  //*check if logged in
+  const redirectLogin = () => {
+    if (!loggedIn) {
+      return <Redirect to="/login" />;
+    }
+  };
+
+  return (
+    <div>
+      {redirectLogin()}
+      <h1>This is home!</h1>
+    </div>
+  );
 }
 
 export default Home;
