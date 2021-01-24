92f927308eb92d4ddaefd40bbe40675db1f4c204
Teo Roca <teo.roca798@gmail.com>
2021-01-24 19:30:18 +0100

    añade prototipo componente Product

--- src/pages/Produits.js
+++ src/pages/Produits.js
@@ -1,6 +1,15 @@
-import "../styles/home.scss";
 import React from "react";
+import styled from "styled-components";
+import { Product } from "../components/";
+
+const Wrapper = styled.div`
+  min-height: 100%;
+`;
 
 export default function Produits() {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <Product />
+    </Wrapper>
+  );
 }
