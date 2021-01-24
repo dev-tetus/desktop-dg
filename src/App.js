0a37caf9fb6dd28f81ea1cb6d7ada3ac7eff61a5
Teo Roca <teo.roca798@gmail.com>
2021-01-24 19:33:09 +0100

    Refactoriza código
    La lógica de redireccionamiento se traslada a app.js
    ya que se va a usar PageNav.js como plantilla para las páginas de
    navegación de la app, por tanto el código de la línea 22 en adelante
    se traslada a PageNav.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import "./App.scss";
+import "./styles/home.scss";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
-
-import { Home, Login } from "./pages/";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
-import { toggleAuth } from "./redux/actions/index";
 import axios from "./config/axios";
+import { toggleAuth } from "./redux/actions/index";
+
+import { Login, PageNav } from "./pages/";
 
 function App() {
+  const isLogged = useSelector((state) => state.authentication.isAuth);
   const dispatch = useDispatch();
 
+  //TODO: Cache session in localStorage or something while it's active
+  //* check if session active
   const checkSession = async () => {
     try {
       const response = await axios({
@@ -24,17 +28,25 @@ function App() {
       if (error) console.log(error.response.data.message);
     }
   };
+
+  //*every time page refresh we check session to persist in redux store
   useEffect(() => {
     window.addEventListener("load", checkSession());
     return () => {
       window.removeEventListener("load", checkSession());
     };
   });
+
   return (
-    <Router>
+    <Router className="router-wrapper">
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/login" exact component={Login} />
+        {isLogged ? (
+          <Route path="/" component={PageNav} />
+        ) : (
+          <Route exact path="/" component={Login} />
+        )}
+
+        <Route path="*" render={() => <h1>Not found</h1>} />
       </Switch>
     </Router>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import React from "react";
 
-import logo from "../assets/svg/mon logo 1.png";
-import { Nav } from "../components/index";
 import "../styles/home.scss";
 
 function Home() {
-  const isLogged = useSelector((state) => state.authentication.isAuth);
-
-  //*check if logged in
-  const redirectLogin = () => {
-    if (!isLogged) {
-      return <Redirect to="/login" />;
-    }
-  };
-
   return (
     <div>
-      {redirectLogin()}
-      <img className="logo" src={logo} />
-      <div className="glass">
-        <Nav className="nav" />
-      </div>
+      <h1>hello from home</h1>
     </div>
   );
 }
